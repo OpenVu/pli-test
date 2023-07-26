e6f6b347e6d8e5613e9b88b2b3eeba2faa7da77b
RedouaneTM@ <redouane343@yahoo.com>
2023-07-26 15:17:39 +0100

    Update elistbox.h

--- lib/gui/elistbox.h
+++ lib/gui/elistbox.h
@@ -65,7 +65,7 @@ struct eListboxStyle
 {
 	ePtr<gPixmap> m_background, m_selection, m_shadow;
 	int m_transparent_background;
-	gRGB m_background_color, m_background_color_selected,
+	gRGB m_background_color, m_background_color_global, m_background_color_selected,
 	m_foreground_color, m_foreground_color_selected, m_border_color, m_sliderborder_color, m_sliderforeground_color;
 	int m_background_color_set, m_background_color_global_set, m_foreground_color_set, m_background_color_selected_set, m_foreground_color_selected_set, m_sliderforeground_color_set, m_sliderborder_color_set, m_scrollbarsliderborder_size_set;
 	int m_shadow_set;	
