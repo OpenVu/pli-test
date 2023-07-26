377477897cfe75a3f4d9a3684f7b22bf66f95c9b
RedouaneTM@ <redouane343@yahoo.com>
2023-07-26 15:24:20 +0100

    Update elistbox.h

--- lib/gui/elistbox.h
+++ lib/gui/elistbox.h
@@ -67,6 +67,7 @@ struct eListboxStyle
 	int m_transparent_background;
 	gRGB m_background_color, m_background_color_global, m_background_color_selected,
 	m_foreground_color, m_foreground_color_selected, m_border_color, m_sliderborder_color, m_sliderforeground_color;
+	gRGB m_background_col_current = m_background_color;
 	int m_background_color_set, m_background_color_global_set, m_background_color_rows_set, m_foreground_color_set, m_background_color_selected_set, m_foreground_color_selected_set, m_sliderforeground_color_set, m_sliderborder_color_set, m_scrollbarsliderborder_size_set;
 	int m_shadow_set;	
 		/*
